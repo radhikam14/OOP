cb9f739ee25b7ece6eb2b24c2cdf3ee4caa28287
radhikam14 <52630399+radhikam14@users.noreply.github.com>
2019-08-03 14:06:24 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: Admin
+ *
+ * Created on 28 July, 2019, 12:38 PM
+ */
+
+#include <cstdlib>
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{ 
+    int i= 20;
+    float r;
+    r = i*0.01;
+    cout<<r;
+    
+    
+    return 0;
+}
+
